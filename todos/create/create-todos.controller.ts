5dd1029f324a7bd68782f6197a0b6b378883ffcf
namj <61493056+Namra-Jabeen@users.noreply.github.com>
2022-02-16 11:35:13 +0100

    Done with create and list endpoints

--- todos/create/create-todos.controller.ts
+++ todos/create/create-todos.controller.ts
@@ -3,6 +3,11 @@ import { ITodo } from "../../interfaces/ITodos";
 import { createTodo } from "./create-todos";
 
 export async function createTodosController(req: Request, res: Response) {
-  const newTodo: ITodo = await createTodo(req.body);
-  res.send(newTodo);
+  try {
+    const newTodo: ITodo = await createTodo(req.body);
+    res.send(newTodo);
+  } catch (err) {
+    console.error(err);
+    res.send(500, { message: "something doesnt work" });
+  }
 }
--- todos/create/create-todos.ts
+++ todos/create/create-todos.ts
@@ -7,25 +7,22 @@ export async function createTodo(newTodo: ITodo): Promise<ITodo> {
     id: uuidv4(),
     ...newTodo,
   };
-  let mysql = require("mysql");
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Jjt82sqq",
-    database: "TodoDatabase",
-  });
-  connection.connect();
 
-  let status_query = `SELECT id FROM Status where status.Status = '${todo.status}';`;
-  connection.query(status_query, function (err, rows) {
-    if (err) throw err;
+  let statusId = await connection.query(
+    `SELECT id as status_id FROM Status where status.Status = '${todo.status}';`
+  );
 
-    todo.status = rows[0];
-  });
-  let sql_query = `INSERT INTO Todo VALUES ('${todo.id}',${todo.status},1,'${todo.assignee}','${todo.assigned}','${todo.title}','${todo.description}','${todo.dateAdded}','${todo.dateCompleted}');`;
-  connection.query(sql_query, function (err, rows, fields) {
-    if (err) throw err;
-  });
-  connection.end();
+  let sql_create_query = `INSERT INTO Todo 
+VALUES ('${todo.id}',
+${statusId[0].status_id},
+${todo.category},
+'${todo.assignee}',
+'${todo.assigned}',
+'${todo.title}',
+'${todo.description}',
+'${todo.dateAdded}',
+'${todo.dateCompleted}');`;
+
+  await connection.query(sql_create_query);
   return Promise.resolve(todo);
 }
